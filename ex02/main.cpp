c6d1c804f512fb449690eb4b2e6ee91b3ddb17a9
Boumlik-Brahim <boumlik01brahim@gmail.com>
2023-03-24 18:14:16 +0000

    Fix: ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bbrahim <bbrahim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 08:45:01 by bbrahim           #+#    #+#             */
-/*   Updated: 2023/03/24 15:46:41 by bbrahim          ###   ########.fr       */
+/*   Updated: 2023/03/24 18:13:41 by bbrahim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,7 +115,58 @@ void sortDeque(std::deque<int> &v, int start, int size)
 /*------------------------------Deque-----------------------------------*/
 
 /*------------------------------Vector----------------------------------*/
- 
+
+void printVector(std::vector<int> &v, char c)
+{
+	if(c == 'b')
+		std::cout << "Before:	";
+	else if (c == 'a')
+		std::cout << "After:	";
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		std::cout << v.at(i) << " ";
+	}
+	std::cout << std::endl;
+}
+
+int is_digit(std::string str)
+{
+	for( size_t i = 0; i < str.size(); i++)
+	{
+		if (!std::isdigit(str[i]))
+			return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
+
+int checkSort(std::vector<int> &v)
+{
+	for (size_t i = 0; i < v.size() - 1; i++)
+	{
+		for (size_t j = i + 1; j < v.size(); j++)
+		{
+			if (v.at(i) < v.at(j))
+				break ;
+			else 
+				return (EXIT_FAILURE);
+		}
+	}
+	return (EXIT_SUCCESS);
+}
+
+int checkdup(std::vector<int> &v)
+{
+	for (size_t i = 0; i < v.size() - 1; i++)
+	{
+		for (size_t j = i + 1; j < v.size(); j++)
+		{
+			if (v.at(i) == v.at(j))
+				return (EXIT_SUCCESS);
+		}
+	}
+	return (EXIT_FAILURE);
+}
+
 void mergeVector(std::vector<int> &v, int start, int half, int size)
 {
 	int n1 = half - start + 1;
@@ -215,64 +266,41 @@ void sortVector(std::vector<int> &v, int start, int size)
 
 int main(int argc, char const *argv[])
 {
-	std::vector<int> v1;
+	std::vector<int>	v1;
+	std::deque<int>		v2;
+	int					val;
 
 	clock_t start_vector = clock();
 	for (int i = 1; i < argc; i++)
 	{
-		v1.push_back(atoi(argv[i]));
+		val = atoi(argv[i]);
+		if (!is_digit(argv[i]) || val < 0)
+		{
+			std::cout << "Error" << std::endl;
+			return (EXIT_FAILURE);
+		}
+		v1.push_back(val);
 	}
-
-	std::cout << "Before: ";
-	for (size_t i = 0; i < v1.size(); i++)
+	if (!checkSort(v1) || !checkdup(v1))
 	{
-		std::cout << v1[i] << " ";
+		std::cout << "Error" << std::endl;
+		return (EXIT_FAILURE);
 	}
-	std::cout << std::endl;
-
+	printVector(v1, 'b');
 	sortVector(v1, 0, v1.size());
 	clock_t end_vector = clock();
-
 	double elapsed_time_vector = double(end_vector - start_vector) / CLOCKS_PER_SEC * 1000000;
+	printVector(v1, 'a');
+	
+	clock_t start_deque = clock();
+	for (int i = 1; i < argc; i++)
+		v2.push_back(atoi(argv[i]));
+	sortDeque(v2, 0, v2.size());
+	clock_t end_deque = clock();
+	double elapsed_time_deque = double(end_deque - start_deque) / CLOCKS_PER_SEC * 1000000;
 
-	std::cout << "After: ";
-	for (size_t i = 0; i < v1.size(); i++)
-	{
-		std::cout << v1[i] << " ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "Time to process a range of " << v1.size() << " elements with std::vector<int> v1 : " << std::fixed << std::setprecision(5) << elapsed_time_vector << " us"<< std::endl;
-
-
-	// std::deque<int> v2;
-	// clock_t start_deque = clock();
-	// for (int i = 1; i < argc; i++)
-	// {
-	// 	v2.push_back(atoi(argv[i]));
-	// }
-
-	// std::cout << "Before: ";
-	// for (size_t i = 0; i < v2.size(); i++)
-	// {
-	// 	std::cout << v2[i] << " ";
-	// }
-	// std::cout << std::endl;
-
-	// sortDeque(v2, 0, v2.size());
-	// clock_t end_deque = clock();
-
-	// double elapsed_time_deque = double(end_deque - start_deque) / CLOCKS_PER_SEC * 1000000;
-
-	// std::cout << std::endl;
-	// std::cout << "After: ";
-	// for (size_t i = 0; i < v2.size(); i++)
-	// {
-	// 	std::cout << v2[i] << " ";
-	// }
-	// std::cout << std::endl;
-
-	// std::cout << "Time to process a range of " << v2.size() << " elements with std::deque<int> v2 : " << std::fixed << std::setprecision(5) << elapsed_time_deque << " us"<< std::endl;
+	std::cout << "Time to process a range of	" << v1.size() << " elements with std::vector<int> v1 : " << std::fixed << std::setprecision(5) << elapsed_time_vector << " us"<< std::endl;
+	std::cout << "Time to process a range of	" << v2.size() << " elements with std::deque<int> v2 : " << std::fixed << std::setprecision(5) << elapsed_time_deque << " us"<< std::endl;
 
 	return (0);
 }
